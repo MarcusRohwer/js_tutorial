25ad49524c4fcdc3936e177cf14ebc7daacfdf8c
Marcus Rohwer <marcusrohwer@gmx.de>
2018-09-04 18:00:41 +0200

    functional programming and objects

--- palindrome.js
+++ palindrome.js
@@ -1,11 +1,44 @@
 
-// Reverses a string.
-function reverse(string) {
-  return Array.from(string).reverse().join("");
+// Reverses a string. => prototype: inheritance
+String.prototype.reverse = function() {
+  return Array.from(this).reverse().join("");
 }
 
-// Returns true for a palindrome, false otherwise.
-function palindrome(string) {
-  string = string.toLowerCase();
-  return string === reverse(string);
+String.prototype.blank = function() {
+  let regex = new RegExp(/^\s*$/);
+  return regex.test(this);
 }
+
+
+// Defines a Phrase object.
+function Phrase(content) {
+  this.content = content;
+
+  this.processor = function(string) {
+    return string.toLowerCase();
+  }
+
+ // Returns content processed for palindrome testing.
+ this.processedContent = function() {
+   return this.processor(this.content);
+ }
+
+  // Returns true for a palindrome, false otherwise.
+  this.palindrome = function palindrome() {
+    return this.processedContent() === this.processedContent().reverse();
+  }
+}
+
+
+// Defines a TranslatedPhrase object.
+function TranslatedPhrase(content, translation) {
+  this.content = content;
+  this.translation = translation;
+
+  // Returns translation processed for palindrome testing.
+  this.processedContent = function processedContent() {
+    return this.processor(this.translation);
+  }
+}
+
+TranslatedPhrase.prototype = new Phrase();
