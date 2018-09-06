9a2697d8aa366e468af20fa953b74e6c21b3a807
Marcus Rohwer <marcusrohwer@gmx.de>
2018-09-06 15:18:25 +0200

    further improvements on the palindrome tester

--- main.js
+++ main.js
@@ -1,10 +1,27 @@
 let Phrase = require("marc-palindrome");
 
-let string = prompt("Please enter a string for palindrome testing:");
-let phrase = new Phrase(string);
+function palindromeTester() {
+  let string = prompt("Please enter a string for palindrome testing:");
+  let phrase = new Phrase(string);
+  let palindromeResult = document.querySelector("#palindromeResult");
 
-if (phrase.palindrome()) {
-  alert(`"${phrase.content}" is a palindrome!`);
-} else {
-  alert(`"${phrase.content}" is not a palindrome.`)
+  if (phrase.palindrome()) {
+    palindromeResult.innerHTML = `"<strong>${phrase.content}</strong>" is a palindrome!`;
+  } else {
+    palindromeResult.innerHTML = `"<strong>${phrase.content}</strong>" is not a palindrome.`;
+  }
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+  let button = document.querySelector("#palindromeTester");
+  button.addEventListener("click", function() {
+    palindromeTester();
+  });
+});
+
+// document.addEventListener("DOMContentLoaded", function() {
+//   let form = document.querySelector("#palindromeTester");
+//   form.addEventListener("submit", function() {
+//     palindromeTester();
+//   });
+// });
